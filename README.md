cc7357e53ac1bea2ca88fc8f97bdb5bf6429b96b
Vitoria Oliveira <viklic@gmail.com>
2021-12-08 08:54:30 -0500

    addition changes made to the project with the addiction of  a function to display one's birth sign information when they enter their day and month of  birth.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-Beginning of the website Tziporah
+This is a business webpage for Tziporah. 
+The main page of the site is a presentaton of the business owner
+The 'Your Birth Sign' page shows the birth sign and it's information based on the date entered on the form.
+The contact me page contains pictures of past cerimonies and contact information of the page's owner
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* Function for hamburger menu */
+
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -11,3 +13,49 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener
         hamburger.classList.remove("active");
         navMenu.classList.remove("active");
     }))
+/*-------------------------------------------------*/
+/*Function for zodiac information */
+
+function signo() {
+    let dia, mes;
+    dia = parseInt(document.zodform.dia.value);
+    mes = parseInt(document.zodform.mes.value);
+
+    var image = document.getElementById("zodImages"); 
+    if ((dia>=21&&mes==3)||(dia<=20&&mes==4)) {
+        document.getElementById("zodImages").src="/bsigns/aries.jpg";
+    }
+    if ((dia>=24&&mes==9)||(dia<=23&&mes==10))
+        document.getElementById("zodImages").src="/bsigns/libra.jpg"
+    
+    if ((dia>=21&&mes==4)||(dia<=21&&mes==5))
+        document.getElementById("zodImages").src="/bsigns/taurus.jpg"
+    
+    if ((dia>=24&&mes==10)||(dia<=22&&mes==11))
+        document.getElementById("zodImages").src="/bsigns/scorpio.jpg"
+    
+    if ((dia>=22&&mes==5)||(dia<=21&&mes==6))
+        document.getElementById("zodImages").src="/bsigns/gemini.jpg"
+
+    if ((dia>=23&&mes==11)||(dia<=21&&mes==12))
+        document.getElementById("zodImages").src="/bsigns/sagittarius.jpg"
+
+    if ((dia>=21&&mes==6)||(dia<=23&&mes==7))
+        document.getElementById("zodImages").src="/bsigns/cancer.jpg"
+
+    if ((dia>=22&&mes==12)||(dia<=20&&mes==1))
+        document.getElementById("zodImages").src="/bsigns/capricorn.jpg"
+
+    if ((dia>=24&&mes==7)||(dia<=23&&mes==8))
+        document.getElementById("zodImages").src="/bsigns/leo.jpg"
+
+    if ((dia>=21&&mes==1)||(dia<=19&&mes==2))
+        document.getElementById("zodImages").src="/bsigns/aquarius.jpg"
+
+    if ((dia>=24&&mes==8)||(dia<=23&&mes==9))
+        document.getElementById("zodImages").src="/bsigns/virgo.jpg"
+
+    if ((dia>=20&&mes==2)||(dia<=20&&mes==3))
+        document.getElementById("zodImages").src="/bsigns/pisces.jpg"
+
+}
